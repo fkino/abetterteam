d12f1332f45af96004d379c79540f83e7bf20995
Fumihiko Kinoshita <fkinos@gmail.com>
2010-01-10 22:36:55 +0900

    generate permalink url

--- abetterteam.rb
+++ abetterteam.rb
@@ -2,12 +2,28 @@ require 'rubygems'
 require 'sinatra'
 require 'haml'
 require 'yaml'
+require 'base64'
 
 helpers do
   def load_quiz
     open("quiz.yml") do |f|
       @quiz = YAML.load(f.read)
     end
+  end
+
+  def encode_param(params)
+    Base64.encode64(params.map do |key, value|
+      "#{key}=#{value}"
+    end.join("&"))
+  end
+
+  def decode_param(str)
+    params = {}
+    Base64.decode64(str).split("&").each do |param|
+      key,value = param.split("=")
+      params[key] = value
+    end
+    params
   end
 end
 
@@ -20,19 +36,25 @@ get '/' do
   haml :index
 end
 
-get '/input' do
+get '/new' do
   load_quiz
   haml :input
 end
 
 post '/result' do
+  redirect '/quiz/' + encode_param(params)
+end
+
+get '/quiz/:result' do
   load_quiz
+
+  @result = decode_param(params["result"])
 
   @quiz.each do |quiz|
     point = 0
     quiz["items"].each_with_index do |item, index|
       params_index = "#{quiz["category"]}#{index}"
-      point += item["#{params[params_index]}_point"].to_i
+      point += item["#{@result[params_index]}_point"].to_i
     end
     quiz["point"] = point.to_s
   end
